252d86f2fc6b85b7967cd40849cb0413aa319feb
kaka166 <hafizhfaruq858@gmail.com>
2022-04-23 11:19:35 +0700

    first commit

---
+++ resources/views/gud/index.blade.php
@@ -0,0 +1,51 @@
+@extends('template')
+@section('content')
+    <div class="row mt-5 mb-5">
+        <div class="col-lg-12 margin-tb">
+            <div class="float-left">
+                <h2>Gudang</h2>
+            </div>
+            <div class="float-right">
+                <a class="btn btn-success" href="{{ route('gud.create') }}"> Input Data</a>
+            </div>
+        </div>
+    </div>
+
+    @if ($message = Session::get('succes'))
+    <div class="alert alert-success">
+        <p>{{ $message }}</p>
+    </div>
+    @endif
+
+    <table class="table table-bordered">
+        <tr>
+            <th width="20px" class="text-center">Tanggal</th>
+            <th>Nama Pegawai</th>
+            <th width="280px"class="text-center">Nama Barang</th>
+            <th width="280px"class="text-center">Jumlah</th>
+            <th width="280px"class="text-center">Keterangan</th>
+            <th width="280px"class="text-center">Action</th>
+        </tr>
+        @foreach ($gud as $gudang)
+        <tr>
+            <td class="text-center">{{ ++$i }}</td>
+            <td>{{ $gudang->NamaPegawai }}</td>
+            <td>{{ $gudang->NamaBarang }}</td>
+            <td>{{ $gudang->Jumlah }}</td>
+            <td>{{ $gudang->Keterangan }}</td>
+            <td class="text-center">
+                <form action="{{ route('gud.destroy',$gudang->id) }}" method="POST">
+
+                    <a class="btn btn-primary btn-sm" href="{{ route('gud.edit',$gudang->id) }}">Edit</a>
+
+                    @csrf
+                    @method('DELETE')
+
+                </form>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+
+    {!! $gud->links() !!}
+@endsection
